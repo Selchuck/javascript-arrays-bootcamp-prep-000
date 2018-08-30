2b9461a29babecdaf056b1b321fc894f4797f746
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-08-30 12:20:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -47,5 +47,5 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  array.slice(array.length)
+  return array.slice(0, array.length-1)
 }
